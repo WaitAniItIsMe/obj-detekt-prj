e01586a156487443bcf7ff5ee0fb798e6b2bd6e7
WaitAniItIsMe <82379412+WaitAniItIsMe@users.noreply.github.com>
2022-05-29 17:08:13 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,47 @@
+img = "";
+status = "";
+objects = [];
+function preload() {
+    img = loadImage('room.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status !="")
+    {
+        for (i = 0; i <objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects detected";
+
+            document.getElementById("numberOf").innerHTML = "Number of objects detected: " + objects.length;
+            fill("red");
+            percent = Math.floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y)
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
